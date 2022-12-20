bc8a9f0bebb9bad089055321de0863ca5dc5b88c
Leah Plummer <leahplumm@gmail.com>
2022-12-19 18:51:15 -0600

    output statement updated

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -7,17 +7,53 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const NumToWord = (roll) => {
+    let dieString = ""
+    
+    switch (roll)
+    {
+        
+        case 1:
+            dieString = "one"; // These \uXXXX values are fancy unicode characters
+            break;
+        case 2:
+            dieString = "two";
+            break;
+        case 3:
+            dieString = "three";
+            break;
+        case 4:
+            dieString = "four";
+            break;
+        case 5:
+             dieString = "five";
+            break;
+        case 6:
+             dieString = "six";
+             break;
+    }
+     return dieString;       
+}
+
 for (let i = 0; i < 10; i++) {
-    let rollOne = getRandomInt(1, 6)
-    let rollTwo = getRandomInt(1, 6)
+    let rollOne = getRandomInt(1, 6);
+    let r1 = NumToWord(rollOne); 
+    let rollTwo = getRandomInt(1, 6);
+    let r2 = NumToWord(rollTwo);
     let doubles = ""
+    
     if (rollOne == rollTwo) {
         doubles = "DOUBLES!"
     }
+    
+    console.log(`${r1} + ${r2} == ${rollOne + rollTwo} ${doubles}`)
+}
 
-    console.log(`${rollOne} + ${rollTwo} == ${rollOne + rollTwo} ${doubles}`)
+
+
+
+
     
 
 
-}
 
